563098240717d5461a4c0cc246d3b414d897055b
Max DePriest <maxd924@gmail.com>
2016-01-24 11:47:58 -0600

    event prevent

--- jquery.js
+++ jquery.js
@@ -16,12 +16,15 @@ $(document).ready(function () {
 			success: function (data, textStatus, xhr) {
 				$('#results').html('');
 				for (var i = 0; i < 5; i++) {
+					console.log(data);
 					$("#results").append("<li>"+ data['storeLocation'][i]['storeName'] + " " + data['storeLocation'][i]['address1'] + ", " + data['storeLocation'][i]['city'] + ', ' + data['storeLocation'][i]['state'] + " " + data['storeLocation'][i]['zip'] + " " + data['storeLocation'][i]['phone'] + " " + "<input type='button' class='my-store btn-default close-button' data-dismiss='modal' id=" + data['storeLocation'][i]['storeNumber'] + " value='My Store'" + "</li>");
 				};
 				$(".my-store").click(function() {
 					var storeNum = $(this).attr('id');
 					console.log(storeNum);
-					$("#product-search").click(function () {
+					$("#product-search").click(function (event) {
+						event.preventDefault();
+						console.log(storeNum + " ");
 						$.ajax({
 							url: "http://api.lowes.com/product/keyword?keyword=" + $('#product').val() + "&storeNumber=" + storeNum + "&maxResults=10&priceFlag=status&api_key=pmpn9ygd676cpzsngnwhd68p",
 							headers: {
@@ -31,6 +34,9 @@ $(document).ready(function () {
 							dataType: 'json',
 							data: '',
 							success: function (data, textStatus, xhr) {
+								console.log(data);
+								console.log(textStatus);
+								console.log(xhr);
 								$('#product-results').html('');
 								for (var i = 0; i < 10; i++) {
 									$("#product-results").append("<li>"+ data['productList'][i]['brand'] + " " + data['productList'][i]['description'] + ", " + "$" + data['productList'][i]['networkPrice'] + " " + " " + "<img src='" + data['productList'][i]['imageUrls']['sm'] + "'>" + "</li>");
